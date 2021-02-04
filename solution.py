9e0b12f87e959f46b1b39f59228393ead407321c
n0tspam <wgord31@gmail.com>
2021-02-04 02:12:54 -0500

    webserver

--- solution.py
+++ solution.py
@@ -10,21 +10,18 @@ def webServer(port=13331):
     serverSocket.listen(1)
     while True:
         # Establish the connection
-        print('Ready to serve...')
+
         connectionSocket, addr = serverSocket.accept()
         try:
             message = connectionSocket.recv(1024)
-            print('message ', message)
             filename = message.split()[1]
             f = open(filename[1:])
             outputdata = f.read()
-            print('outputdata ', outputdata)
 
             # Send one HTTP header line into socket
             #Fill in start
             connectionSocket.send('HTTP/1.1 200 OK\r\n'.encode())
             #Fill in end
-            print('in try2')
             # Send the content of the requested file to the client
             for i in range(0, len(outputdata)):
                 connectionSocket.send(outputdata[i].encode())
@@ -32,13 +29,10 @@ def webServer(port=13331):
             connectionSocket.send("\r\n".encode())
             connectionSocket.close()
         except IOError:
-            print('in ioerror')
             # Send response message for file not found (404)
             #Fill in start
-            connectionSocket.send(
-                "HTTP/1.1 404 Not Found".encode())
+            connectionSocket.send("HTTP/1.1 404 Not Found".encode())
             #Fill in end
-            print('in try3')
             # Close client socket
             #Fill in start
             connectionSocket.close()
