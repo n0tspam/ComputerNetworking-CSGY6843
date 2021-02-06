fdea6cb5d1acb022c8c8e00b89f934c150324a2a
n0tspam <wgord31@gmail.com>
2021-02-05 23:54:55 -0500

    smtp

--- solution.py
+++ solution.py
@@ -10,16 +10,17 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
 
     recv = clientSocket.recv(1024).decode()
     if recv[:3] != '220':
-        print('220 reply not received from server.')
+        continue
+        #print('220 reply not received from server.')
 
     # Send HELO command and print server response.
     heloCommand = 'HELO Alice\r\n'
     clientSocket.send(heloCommand.encode())
     recv1 = clientSocket.recv(1024).decode()
-    print("Message after EHLO command:" + recv1)
-    print(recv1)
+
     if recv1[:3] != '250':
-        print('250 reply not received from server.')
+        continue
+        #print('250 reply not received from server.')
 
    # Send MAIL FROM command and print server response.
     mailFrom = "MAIL FROM:<wg2159@nyu.edu>\r\n"
